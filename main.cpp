99e32f6641064804ccd5de4f162f1524af733a9b
aabanakhtar-github <aabanakhtar.home@gmail.com>
2023-06-15 14:58:58 -0700

    Made a class foo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+
 int RNG(int, int);
+
+class foo 
+{
+    public:
+        static void do_stuff()
+        {
+            std::cout << "Did something haha " << RNG(1, 6) << std::endl;
+        }
+};
+
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
     std::cout << "Random num : " << RNG(3, 9) << std::endl;
+    foo().do_stuff();
     return 0;
 }
